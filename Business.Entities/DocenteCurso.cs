0cf95add593461560772fbd95bafba2097cb3f35
FedericoUTN <hulalaoooo@gmail.com>
2022-05-18 12:52:22 -0300

    ok

---
+++ Business.Entities/DocenteCurso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class DocenteCurso : BusinessEntity
+    {
+        public TiposCargos Cargo { get; set; }
+        public int IDCurso { get; set; }
+        public int IDDocente { get; set; }
+        public DocenteCurso()
+        {
+            this.Cargo = TiposCargos.Docente;
+        }
+        public enum TiposCargos
+        {
+            Docente,
+            Auxiliar,
+            JefeCatedra
+        }
+    }
+}
---
+++ Business.Entities/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Persona : BusinessEntity
+    {
+        public Persona()
+        {
+            this.TipoPersona = TiposPersonas.Alumno;
+        }
+        public string? Apellido { get; set; }
+        public string? Nombre { get; set;}
+
+        public string? Email { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+        public int IDPlan { get; set; }
+        public int Legajo { get; set; }
+        public string? Telefono { get; set; }
+        public TiposPersonas TipoPersona { get; set; }
+
+        public enum TiposPersonas
+        {
+            Alumno,
+            Docente,
+            Administrador
+        }
+    }
+}
